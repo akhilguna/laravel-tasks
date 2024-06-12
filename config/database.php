ad5fb55278eb9749747a12b2866b8a443a16d1dc
akhilguna <akhilguna96@gmail.com>
2024-06-12 13:01:16 +0530

    commit and chnaged redis

--- config/database.php
+++ config/database.php
@@ -46,7 +46,7 @@ return [
         'mysql' => [
             'driver' => 'mysql',
             'url' => env('DATABASE_URL'),
-            'host' => env('DB_HOST', '127.0.0.1'),
+            'host' => env('DB_HOST', 'phpmysql-server.mysql.database.azure.com'),
             'port' => env('DB_PORT', '3306'),
             'database' => env('DB_DATABASE', 'forge'),
             'username' => env('DB_USERNAME', 'forge'),
@@ -130,7 +130,7 @@ return [
 
         'default' => [
             'url' => env('REDIS_URL'),
-            'host' => env('REDIS_HOST', '127.0.0.1'),
+            'host' => env('REDIS_HOST', 'phpmysql-cache.redis.cache.windows.net'),
             'username' => env('REDIS_USERNAME'),
             'password' => env('REDIS_PASSWORD'),
             'port' => env('REDIS_PORT', '6379'),
@@ -140,7 +140,7 @@ return [
         'cache' => [
             'scheme' => 'tls',
             'url' => env('REDIS_URL'),
-            'host' => env('AZURE_REDIS_HOST', '127.0.0.1'),
+            'host' => env('AZURE_REDIS_HOST', 'phpmysql-cache.redis.cache.windows.net'),
             'username' => env('REDIS_USERNAME'),
             'password' => env('AZURE_REDIS_PASSWORD'),
             'port' => env('AZURE_REDIS_PORT', '6379'),
